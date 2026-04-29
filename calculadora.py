0b7235f62c627b838127e4016f2d2954eb177b2f
Wualofficial <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>
2026-04-29 18:17:09 -0300

    Etapa 1 Soma

---
+++ calculadora.py
@@ -0,0 +1,7 @@
+class operacoes:
+    
+    def __init__(self):
+        self.num1 = 0.0
+    
+    def soma(a, b):
+        return a + b
--- main.py
+++ main.py
@@ -1,13 +1,67 @@
 import flet as ft
-
+from calculadora import operacoes
 
 def main(page: ft.Page):
-    page.title = "Hello World"
+    page.title = "CALC BASIC PYTHON FLET"
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    
+    calc = operacoes()
+    text = ft.Text("0", size=40, weight=ft.FontWeight.BOLD)
 
-    page.add(ft.Text("Hello, World!", size=40, weight=ft.FontWeight.BOLD))
+    def update_display(value):
+        if value == "." and "." in text.value:
+            text.value = text.value
+        elif value == "+":
+            if calc.num1 == 0.0:
+                calc.num1 = float(text.value)
+                text.value = "0"
+                print(calc.num1)
+            else:
+                resultado = operacoes.soma(calc.num1, float(text.value))
+                calc.num1 = resultado 
+                text.value = str(resultado)
+                
+        
+        else:
+            text.value = text.value + value if text.value != "0" else value
+            
+        
+        page.update()
+    
+    def clear_display():
+        text.value = "0"
+        calc.num1 = 0.0
+        page.update()
+        
+    def save_Substract(value,substract=None):
+        
+        if substract == None:
+            substract = float(value)
+            text.value = "0"
+        else:
+            substract -= float(value)  
+            text.value = f"{substract}"
+            print(substract)
+        page.update()
+
+    row1 = ft.Row(controls=[ft.ElevatedButton("1", on_click=lambda e: update_display("1")), ft.ElevatedButton("2", on_click=lambda e: update_display("2")), ft.ElevatedButton("3", on_click=lambda e: update_display("3"))],alignment=ft.MainAxisAlignment.CENTER)
+    row2 = ft.Row(controls=[ft.ElevatedButton("4", on_click=lambda e: update_display("4")), ft.ElevatedButton("5", on_click=lambda e: update_display("5")), ft.ElevatedButton("6", on_click=lambda e: update_display("6"))],alignment=ft.MainAxisAlignment.CENTER)
+    row3 = ft.Row(controls=[ft.ElevatedButton("7", on_click=lambda e: update_display("7")), ft.ElevatedButton("8", on_click=lambda e: update_display("8")), ft.ElevatedButton("9", on_click=lambda e: update_display("9"))],alignment=ft.MainAxisAlignment.CENTER)
+    row4 = ft.Row(controls=[ft.ElevatedButton("0", on_click=lambda e: update_display("0")), ft.ElevatedButton(".", on_click=lambda e: update_display(".")), ft.ElevatedButton("=", on_click=lambda e: calculate())],alignment=ft.MainAxisAlignment.CENTER)
+    row5 = ft.Row(controls=[ft.ElevatedButton("C", on_click=lambda e: clear_display()), ft.ElevatedButton("-", on_click=lambda e: update_display("-")),ft.ElevatedButton("+", on_click=lambda e: update_display("+"))],alignment=ft.MainAxisAlignment.CENTER)
+
+    page.add(
+        text,
+        row1,
+        row2,
+        row3,
+        row4,
+        row5
+    
+    )
     
     
-if __name__ == "__main__":     
-    ft.app(target=main)
+
+if __name__ == "__main__":
+    ft.run(main)
